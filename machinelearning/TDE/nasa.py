d8dfcc2b68d07c9f1bc28ba481cf07f8b2582ded
EDJI <nathanks223@gmail.com>
2022-11-03 16:45:15 -0300

    adicionado dados e TDE

--- machinelearning/TDE/nasa.py
+++ machinelearning/TDE/nasa.py
@@ -1,4 +1,25 @@
+from re import I
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+from glob2 import glob
+import os
+import re
+folder = 'machinelearning/1st_test/1st_test/*'
+path = 'machinelearning/1st_test/1st_test/'
 
-#Lendo o arquivo
-data_read = pd.read_txt('ChildrenData.csv')
+
+file = glob(folder)
+print(file[0])
+
+for i in file:
+    print(i)
+    df = pd.read_csv(i, sep = '\t')
+    df = pd.DataFrame(i,
+                      columns =['var1', 'var2','var3', 'var3', 'var3', 'var3', 'var3', 'var3', 'var3'])
+    
+
+    print(df.reset_index())    
+
+    break
+
